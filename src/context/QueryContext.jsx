48de64234f85ecf0efb7933b9bc59c2c0da32f36
xvargr <targenta650@gmail.com>
2023-03-16 21:22:50 +0800

    useeffect array deps testing

--- src/context/QueryContext.jsx
+++ src/context/QueryContext.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useMemo } from "react";
 import { useState, createContext } from "react";
 
 import Query from "../utils/Query";
@@ -8,16 +8,40 @@ export const QueryContext = createContext();
 export function QueryContextProvider(props) {
   const [queryInstance] = useState(new Query(""));
   const [currentQuery, setCurrentQuery] = useState(queryInstance.getRawQuery());
+  const [currentExact, setCurrentExact] = useState(queryInstance.exact);
+
+  // useEffect(() => {
+  //   console.log("exact changed");
+  //   setCurrentExact(queryInstance.exact);
+  // }, [queryInstance]);
+
+  // useMemo(() => {
+  //   console.log("exact changed");
+  //   setCurrentExact(queryInstance.exact);
+  // }, [queryInstance.exact]);
 
   function setRawQuery(string) {
     queryInstance.setRawQuery(string);
     setCurrentQuery(string);
   }
 
+  function addExact(string) {
+    queryInstance.addExact(string);
+    setCurrentExact(queryInstance.exact);
+  }
+
+  // function removeExact(string) {
+  //   queryInstance.removeExact(string);
+  //   setCurrentExact(queryInstance.exact);
+  // }
+
   const queryState = {
     queryInstance,
     setRawQuery,
     currentQuery,
+    currentExact,
+    addExact,
+    // removeExact,
   };
 
   return (
--- src/utils/Query.js
+++ src/utils/Query.js
@@ -18,6 +18,40 @@ class Query {
     console.log("query raw is now", input);
     this.rawQuery = input;
   }
+
+  addExact(input) {
+    // console.log("pushing to exact", input);
+    this.exact.push(input);
+  }
+
+  removeExact(input) {
+    console.log("removing from exact", input);
+
+    const index = this.exact.findIndex(input);
+    if (index === -1) {
+      console.error("exact phrase not found");
+      return null;
+    }
+
+    this.exact.splice(1, index);
+  }
+
+  // addExclude(input) {
+  //   console.log("pushing to exclude", input);
+  //   this.exclude.push(input);
+  // }
+
+  // removeExclude(input) {
+  //   console.log("removing from exclude", input);
+
+  //   const index = this.exclude.findIndex(input);
+  //   if (index === -1) {
+  //     console.error("exclude phrase not found");
+  //     return null;
+  //   }
+
+  //   this.exclude.push(input);
+  // }
 }
 
 export default Query;
